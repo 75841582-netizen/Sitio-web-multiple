379d5e24fd979c4b3e5ce607703d3843192da4cf
llanachahuincarlos-sys <llanachahuincarlos@gmail.com>
2026-04-18 09:35:40 -0500

    Login_bienvenido

--- java.js
+++ java.js
@@ -1,3 +1,11 @@
 function abrirServicio(ruta){
     window.location.href = ruta;
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const usuarioGuardado = JSON.parse(localStorage.getItem("usuarioActivo"));
+    const bienvenida = document.getElementById("bienvenidaUsuario");
+
+    if (usuarioGuardado && bienvenida) {
+        bienvenida.textContent = `Bienvenido, ${usuarioGuardado.nombre}`;
+    }
+});
